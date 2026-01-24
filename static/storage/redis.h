fb226e5c997ae86a347ddc098db8ad021220aa3c
Neal Itharaja <sitharaj@ucsc.edu>
2026-01-23 23:54:28 -0800

    Started Redis header

--- static/storage/redis.h
+++ static/storage/redis.h
@@ -5,6 +5,32 @@
 #ifndef PANCAKE_REDIS_H
 #define PANCAKE_REDIS_H
 
+#include <string>
+#include <vector>
+#include <optional>
+#include <cstdint>
+#include <hiredis.h>
+
 using namespace std;
+
+class redisClient {
+    public:
+        redisClient(const string& host, int port);
+        ~redisClient();
+        redisClient(const redisClient&) = delete;
+        redisClient& operator=(const redisClient&) = delete;
+        redisClient(redisClient&&) noexcept;
+        redisClient& operator=(redisClient&&) noexcept;
+        bool put(const string& key, const string& value);
+        optional<string> get(const string& key);
+        bool insert(const string& key, const string& value);
+        uint64_t incr(const string& key);
+        unt64_t decr(const string& key);
+        vector<string> keysScan(const string& key, size_t count);
+        bool update(const string& key, const string& value);
+    private:
+        reddisContext* ctx = nullptr;
+        redisReply* cmd(const char* fmt, ...);
+};
 
 #endif //PANCAKE_REDIS_H
