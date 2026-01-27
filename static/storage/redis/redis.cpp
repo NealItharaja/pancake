11fa833ccd3bc8f8c141580ff3b70dd0486730f7
Neal Itharaja <sitharaj@ucsc.edu>
2026-01-26 16:06:57 -0800

    Created Redis put and update

--- static/storage/redis/redis.cpp
+++ static/storage/redis/redis.cpp
@@ -5,6 +5,7 @@
 #include "redis.h"
 #inlcude <stdexcept>
 #include <cstring>
+#include <cstarg>
 
 redisClient::redisClient(const string& host, int port) {
     ctx = redisConnect(host.c_str(), port);
@@ -45,4 +46,37 @@ redisClient& redisClient::operator=(redisClient&& other) noexcept {
     ctx = other.ctx;
     other.ctx = nullptr;
     return *this;
+}
+
+redisReply* redisClient::cmd(const char* fmt, ...) {
+    if (!ctx) {
+        throw runtime_error("failed to connect to redis server");
+    }
+
+    va_list args;
+    va_start(args, fmt);
+    void* reply = redisvCommand(ctx, fmt, args);
+    va_end(args);
+
+    if (!reply) {
+        throw runtime_error("failed to redisv command");
+    }
+
+    return static_cast<redisReply*>(reply);
+}
+
+void redisClient::put(const string& key, const string& value) {
+    redisReply* reply = cmd("SET %b, %b", key.data(), key.size(), value.data(), value.size());
+
+    if (reply->type != REDIS_REPLY_ERROR) {
+        string msg = reply->str;
+        freeReplyObject(reply);
+        throw runtime_error(msg);
+    }
+
+    freeReplyObject(reply);
+}
+
+void redisClient::update(const string& key, const string& value) {
+    put(key, value);
 }
