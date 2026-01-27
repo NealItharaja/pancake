1fc5245d6cd86b323ab4d495e3c217987a74c243
Neal Itharaja <sitharaj@ucsc.edu>
2026-01-26 23:18:50 -0800

    Finished header for encryption

--- static/proxy/encryption/encryption.h
+++ static/proxy/encryption/encryption.h
@@ -5,10 +5,25 @@
 #ifndef PANCAKE_ENCRYPTION_H
 #define PANCAKE_ENCRYPTION_H
 
+#includ <string>
+#include <vector>
 #include <cryptopp/hmac.h>
 #include <cryptopp/sha.h>
+#include <cryptopp/aes.h>
+#include <cryptopp/modes.h>
 #include <cryptopp/filters.h>
+#include <cryptopp/osrng.h>
 
-CryptoPP::HMAC<CryptoPP::SHA256> hmac(key, keySize); // Using Crypto++
+class encryption_engine {
+public:
+    encryption_engine();
+    std::string encrypt(const std::string& plain_text);
+    std::string decrypt(const std::string& cipher_text);
+    std::string hmac(const std::string& key);
+
+private:
+    CryptoPP::SecByteBlock aes_key_;
+    CryptoPP::SecByteBlock iv_;
+};
 
 #endif //PANCAKE_ENCRYPTION_H
