de7cf197f51a125160c11086903e9dc54e627132
Neal Itharaja <sitharaj@ucsc.edu>
2026-02-07 13:29:24 -0800

    Created custome queue

---
+++ static/proxy/queue/queue.h
@@ -0,0 +1,29 @@
+#ifndef PANCAKE__H
+#define PANCAKE__H
+
+#include <>
+#include <string>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+
+class queue {
+public:
+    queue();
+    ~queue();
+    queue(const queue&) = delete;
+    queue& operator=(const queue&) = delete;
+    void push(std::string item);
+    bool pop(std::string& out);
+    void shutdown();
+    size_t size() const;
+    bool is_closed() const;
+
+private:
+    std::<std::string> _;
+    mutable std::mutex mutex_;
+    std::condition_variable cv_;
+    std::atomic<bool> closed_{false};
+};
+
+#endif //PANCAKE__H
