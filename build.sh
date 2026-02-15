77dd35c78a57d6fdd36c1d9300f15b0ab4fac203
mithileshk <mithileshkowshik@gmail.com>
2026-02-15 15:26:38 -0800

    Changes to build.sh and CMakeLists.tx

--- build.sh
+++ build.sh
@@ -2,20 +2,18 @@
 set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 BUILD_DIR="${ROOT_DIR}/cmakebuild"
 
 echo "[*] Creating build directory at ${BUILD_DIR}"
 mkdir -p "${BUILD_DIR}"
-cd "${BUILD_DIR}"
 
 echo "[*] Configuring with CMake"
-cmake "$@" "${ROOT_DIR}"
+cmake -S "${ROOT_DIR}/static" -B "${BUILD_DIR}"
 
 START=$(date +%s)
 
 echo "[*] Building Pancake"
-make -j"$(nproc)"
+cmake --build "${BUILD_DIR}" -j"$(sysctl -n hw.ncpu)"
 
 END=$(date +%s)
 
