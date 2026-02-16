c0b65104ebfeba7d879135f1c65e886c4b27ee6f
mithileshk <mithileshkowshik@gmail.com>
2026-02-15 17:00:12 -0800

    thrift_helpers.h again

--- static/service/thrift_helpers.h
+++ static/service/thrift_helpers.h
@@ -1,59 +1,91 @@
 #ifndef THRIFT_HELPERS_H
 #define THRIFT_HELPERS_H
 
-#pragma once
-
 #include <memory>
 #include <string>
 #include <stdexcept>
 
-// Thrift transport / protocol
+// Thrift transports / protocols
 #include <thrift/transport/TTransportUtils.h>
 #include <thrift/transport/TSocket.h>
 #include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TBufferTransports.h>
 #include <thrift/protocol/TBinaryProtocol.h>
 
-// Thrift servers (NO concurrency headers in Thrift >= 0.18)
+// Thrift servers (NO thrift/concurrency/* in 0.19)
 #include <thrift/server/TSimpleServer.h>
 #include <thrift/server/TThreadedServer.h>
 #include <thrift/server/TThreadPoolServer.h>
 
 namespace thrift_helpers {
 
-    // ------------------------------------------------------------
-    // Client helpers
-    // ------------------------------------------------------------
-    inline void make_framed_binary_client(
-        const std::string& host,
-        int port,
-        std::shared_ptr<apache::thrift::protocol::TProtocol>& out_proto,
-        std::shared_ptr<apache::thrift::transport::TTransport>& out_transport
-    ) {
-        using namespace apache::thrift;
-        using namespace apache::thrift::transport;
-        using namespace apache::thrift::protocol;
+// ------------------------------------------------------------
+// Client helpers
+// ------------------------------------------------------------
+inline void make_framed_binary_client(
+    const std::string& host,
+    int port,
+    std::shared_ptr<apache::thrift::protocol::TProtocol>& out_proto,
+    std::shared_ptr<apache::thrift::transport::TTransport>& out_transport
+) {
+    using namespace apache::thrift::transport;
+    using namespace apache::thrift::protocol;
 
-        auto socket = std::make_shared<TSocket>(host, port);
-        auto transport = std::make_shared<TFramedTransport>(socket);
-        auto protocol = std::make_shared<TBinaryProtocol>(transport);
+    auto socket = std::make_shared<TSocket>(host, port);
+    auto transport = std::make_shared<TFramedTransport>(socket);
+    auto protocol = std::make_shared<TBinaryProtocol>(transport);
 
-        transport->open();
+    transport->open();
 
-        out_proto = protocol;
-        out_transport = transport;
-    }
+    out_proto = protocol;
+    out_transport = transport;
+}
 
-    // ------------------------------------------------------------
-    // Server helpers
-    // ------------------------------------------------------------
-    inline std::shared_ptr<apache::thrift::transport::TServerTransport>
-    make_server_socket(int port) {
-        using namespace apache::thrift::transport;
-        return std::make_shared<TServerSocket>(port);
-    }
+// ------------------------------------------------------------
+// Server helpers
+// ------------------------------------------------------------
+inline std::shared_ptr<apache::thrift::transport::TServerTransport>
+make_server_socket(int port) {
+    using namespace apache::thrift::transport;
+    return std::make_shared<TServerSocket>(port);
+}
 
-    inline std::shared_ptr<apache::thrift::transport::TTransportFactory>
-    make_framed_factory() {
-        using namespace apache::thrift::transport;
-        return std::make_shared<TFramedTransportFacto_
+inline std::shared_ptr<apache::thrift::transport::TTransportFactory>
+make_framed_factory() {
+    using namespace apache::thrift::transport;
+    return std::make_shared<TFramedTransportFactory>();
+}
+
+inline std::shared_ptr<apache::thrift::protocol::TProtocolFactory>
+make_binary_factory() {
+    using namespace apache::thrift::protocol;
+    return std::make_shared<TBinaryProtocolFactory>();
+}
+
+} // namespace thrift_helpers
+
+// ------------------------------------------------------------
+// Optional Crypto++ helpers
+// ------------------------------------------------------------
+#ifdef HAS_CRYPTOPP
+#include <cryptopp/sha.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/filters.h>
+
+inline std::string sha256_hex(const std::string& input) {
+    using namespace CryptoPP;
+
+    SHA256 hash;
+    std::string digest;
+
+    StringSource ss(input, true,
+        new HashFilter(hash,
+            new HexEncoder(new StringSink(digest), false)
+        )
+    );
+
+    return digest;
+}
+#endif
+
+#endif // THRIFT_HELPERS_H
